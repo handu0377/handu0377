415b4a208d317252cd6d06fe6be9bce3f5510aa8
handu0377 <550466412@qq.com>
2016-12-04 16:43:00 +0800

    Signed-off-by: handu0377 <550466412@qq.com>

--- index.php/down.cmd
+++
@@ -1,8 +0,0 @@
-f:\
-cd F:\GIT\handu0377\handu0377\index.php\
-for /l %%d in (1 1 20) do (
-md %%d
-cd F:\GIT\handu0377\handu0377\index.php\
-wget -O "F:\GIT\handu0377\handu0377\index.php\%%d\index.html" http://handu0377.com/index.php/%%d/
-)
-pause
